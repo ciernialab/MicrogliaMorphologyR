0a47e87027ee4e7741c4e2fbdfe899d461b70d95
jenn-kim <jenn_kim@outlook.com>
2023-09-20 17:44:10 -0400

    updates

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' 2xLPS mouse dataset from frontal cortex, striatum, and hippocampal subregions
 #'
 #' @format ## `data_2xLPS_mouse`
-#' A data frame with 46,104 rows and 35 columns:
+#' A data frame with 43,332 rows and 35 columns:
 #' \describe{
 #'   \item{Antibody}{Microglia marker used in immunofluorescent image}
 #'   \item{MouseID}{Subject identifier}
@@ -13,10 +13,26 @@
 #' }
 "data_2xLPS_mouse"
 
+#' 2xLPS fuzzy k-means soft clustering dataset
+#' mouse microglia cells from frontal cortex, striatum, and hippocampal subregions
+#'
+#' @format ## `data_2xLPS_mouse_fuzzykmeans`
+#' A data frame with 43,332 rows and 43 columns:
+#' \describe{
+#'   \item{Antibody}{Microglia marker used in immunofluorescent image}
+#'   \item{MouseID}{Subject identifier}
+#'   \item{Sex}{Sex}
+#'   \item{Treatment}{PBS vehicle or 2xLPS i.p. injection at 0.5 mg/kg, spaced 24 hours apart}
+#'   \item{BrainRegion}{frontal cortex, striatum, or hippocampus}
+#'   \item{Subregion}{frontal cortex: infralimbic (IL), prelimbic (PL), anterior cingulate cortex (ACC); hippocampus: CA1, CA2, CA3, dentate gyrus (DG); stratium: caudate putamen (CP), nucleus accumbens (NA)}
+#'   ...
+#' }
+"data_2xLPS_mouse_fuzzykmeans"
+
 #' EAE mouse dataset from frontal cortex, striatum, and hippocampal subregions
 #'
 #' @format ## `data_EAE`
-#' A data frame with 46,104 rows and 35 columns:
+#' A data frame with 40,334 rows and 33 columns:
 #' \describe{
 #'   \item{Antibody}{Microglia marker used in immunofluorescent image}
 #'   \item{MouseID}{Subject identifier}
@@ -26,28 +42,3 @@
 #' }
 "data_EAE"
 
-#' Fuzzy k-means dataset from 2xLPS
-#'
-#' @format ## `data_fuzzykmeans`
-#' A data frame with 46,104 rows and 35 columns:
-#' \describe{
-#'   \item{Antibody}{Microglia marker used in immunofluorescent image}
-#'   \item{MouseID}{Subject identifier}
-#'   \item{BrainRegion}{frontal cortex, striatum, or hippocampus}
-#'   \item{Subregion}{frontal cortex: infralimbic (IL), prelimbic (PL), anterior cingulate cortex (ACC); hippocampus: CA1, CA2, CA3, dentate gyrus (DG); stratium: caudate putamen (CP), nucleus accumbens (NA)}
-#'   ...
-#' }
-"data_fuzzykmeans"
-
-#' Fkm dataset from 2xLPS tided up
-#'
-#' @format ## `data_2xLPS_fuzzykmeans`
-#' A data frame with 46,104 rows and 35 columns:
-#' \describe{
-#'   \item{Antibody}{Microglia marker used in immunofluorescent image}
-#'   \item{MouseID}{Subject identifier}
-#'   \item{BrainRegion}{frontal cortex, striatum, or hippocampus}
-#'   \item{Subregion}{frontal cortex: infralimbic (IL), prelimbic (PL), anterior cingulate cortex (ACC); hippocampus: CA1, CA2, CA3, dentate gyrus (DG); stratium: caudate putamen (CP), nucleus accumbens (NA)}
-#'   ...
-#' }
-"data_2xLPS_fuzzykmeans"
